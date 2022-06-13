0995978c978e4a9010508662f4dd6cc2daf2aae1
Guilherme Endlich <g.endlich.g@gmail.com>
2022-06-13 12:00:37 -0300

    feature: now we can change the quantity on the cart

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit} from '@angular/core';
 import { CartService } from '../cart.service';
+import { options } from '../products';
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
@@ -8,6 +9,7 @@ import { CartService } from '../cart.service';
 export class CartComponent implements OnInit {
   items = this.cartService.getItems();
   totalPrice = this.cartService.getTotal();
+  options = options;
 
   constructor(
     private cartService: CartService,
@@ -15,19 +17,24 @@ export class CartComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
-
-
+  
+  
   onReset(): void {
     this.items = this.cartService.clearCart();
     this.totalPrice = this.cartService.getTotal();
     window.alert(`Your cart has been emptied`);
   }
-
+  
   onDeleteButton(i: number) {
     this.cartService.deleteItem(i);
     this.items = this.cartService.getItems();
     this.totalPrice = this.cartService.getTotal();
     window.alert(`Your item has been removed`);
+  }
+  
+  newQuantity = 0;
+  changeQuantity(i: number){
+    this.items[i].quantity = this.newQuantity;
+    this.totalPrice = this.cartService.getTotal();
   }
 }
